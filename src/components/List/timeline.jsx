a00bd3b2dfba3fc771e5c44298920b37f665fd70
Emir <emirdelictbf@gmail.com>
2019-09-17 16:24:36 +0200

    Adding timeline page

---
+++ src/components/List/timeline.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect } from "react";
+import user from "../../static/me.png";
+import { Searchbar, List, ListItem } from "framework7-react";
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const items = [];
+    for (let i = 1; i <= 10000; i += 1) {
+      items.push({
+        title: `Item ${i}`,
+        subtitle: `Subtitle ${i}`
+      });
+    }
+    this.state = {
+      items,
+      vlData: {
+        items: []
+      }
+    };
+  }
+  searchAll(query, items) {
+    const found = [];
+    for (let i = 0; i < items.length; i += 1) {
+      if (
+        items[i].title.toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
+        query.trim() === ""
+      )
+        found.push(i);
+    }
+    return found; // return array with mathced indexes
+  }
+  renderExternal(vl, vlData) {
+    this.setState({ vlData });
+  }
+  render() {
+    return (
+      <div className="Timeline">
+        <Searchbar
+          searchContainer=".virtual-list"
+          searchItem="li"
+          searchIn=".item-title"
+          disableButton={!this.$theme.aurora}
+        ></Searchbar>
+
+        <List className="searchbar-not-found">
+          <ListItem title="Nothing found"></ListItem>
+        </List>
+
+        <List
+          className="searchbar-found"
+          medialList
+          virtualList
+          virtualListParams={{
+            items: this.state.items,
+            searchAll: this.searchAll,
+            renderExternal: this.renderExternal.bind(this),
+            height: this.$theme.ios ? 63 : this.$theme.md ? 73 : 46
+          }}
+        >
+          <ul>
+            {this.state.vlData.items.map((item, index) => (
+              <ListItem
+                key={index}
+                mediaItem
+                link="#"
+                title={item.title}
+                subtitle={item.subtitle}
+                style={{ top: `${this.state.vlData.topPosition}px` }}
+              ></ListItem>
+            ))}
+          </ul>
+        </List>
+      </div>
+    );
+  }
+}
---
+++ src/components/Timeline/timeline.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import badge from "../../static/badge.png";
+import { Icon } from "framework7-react";
+
+export default function TimelineComponent() {
+  const elements = [
+    {
+      year: 2019,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    },
+    {
+      year: 2019,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    },
+    {
+      year: 2018,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    },
+    {
+      year: 2018,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    },
+    {
+      year: 2018,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    },
+    {
+      year: 2018,
+      img: badge,
+      name: "Name des Badges",
+      info: "eventuelle Zusatzinformatonen zum batch"
+    }
+  ];
+
+  return (
+    <div className="Timeline">
+      {/* <img src={user} alt="User avatar" /> */}
+      <div className="timeline">
+        <div className="timeline-item start">
+          <div className="timeline-item-divider">FFOÖ</div>
+        </div>
+
+        {elements.map((item, index) => {
+          return (
+            <div className="timeline-item" key={index}>
+              <div className="timeline-item-date">
+                <span>
+                  {elements[index - 1] && elements[index - 1].year === item.year
+                    ? ""
+                    : item.year}
+                </span>
+              </div>
+              <div className="timeline-item-divider">
+                <Icon className="timeline-icon" f7="share" size="24px"></Icon>
+              </div>
+              <div className="timeline-item-content">
+                <div className="item">
+                  <div className="column">
+                    <div className="image-wrap">
+                      <img src={item.img} alt="User avatar" />
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div>
+                      <div>
+                        <b>{item.name}</b>
+                      </div>
+                      <p>{item.info}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+
+        <div className="timeline-item end">
+          <div className="timeline-item-divider"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
